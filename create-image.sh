90fc1d6c965ead5a75da39b2551bba9c7ec1f925
Kamil Toman <kamil.toman@leafclick.com>
2019-03-11 10:47:11 +0100

    Add --static flag as a repro case for the SEGFAULT.

--- create-image.sh
+++ create-image.sh
@@ -4,4 +4,4 @@ mvn clean package
 
 # Building the image requires at least GraalVM Version 1.0.0-rc13-SNAPSHOT
 
-native-image --enable-all-security-services --report-unsupported-elements-at-runtime -jar /home/katox/dev/graal-pg-client/target/graal-pg-client-1.0-SNAPSHOT.jar
+native-image --no-server --static --enable-all-security-services --report-unsupported-elements-at-runtime -jar /home/katox/dev/graal-pg-client/target/graal-pg-client-1.0-SNAPSHOT.jar
